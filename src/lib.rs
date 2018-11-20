e725a170a5997ace4971b7acda6cb06f8ef7dab9
kevlarr <echo1+git@dvnl.net>
2018-11-19 22:31:28 -0500

    remove wee_alloc

--- src/lib.rs
+++ src/lib.rs
@@ -9,16 +9,6 @@ use std::fmt;
 use cfg_if::cfg_if;
 use wasm_bindgen::prelude::*;
 
-cfg_if! {
-    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-    // allocator.
-    if #[cfg(feature = "wee_alloc")] {
-        extern crate wee_alloc;
-        #[global_allocator]
-        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-    }
-}
-
 #[wasm_bindgen]
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
